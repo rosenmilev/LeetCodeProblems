7c18697c5cba4455121f6348370a1d13af7ddb46
traxx900 <rosenmilev@gmail.com>
2024-02-28 21:58:15 +0200

    Add two numbers problem solution.

---
+++ linked_list_implementation.py
@@ -0,0 +1,43 @@
+class Node:
+	def __init__(self, data=0, next=None):
+		self.data = data
+		self.next = next
+
+
+class LinkedList:
+	def __init__(self):
+		self.head = None
+
+	def append(self, data):
+		new_node = Node(data)
+		if not self.head:
+			self.head = new_node
+			return
+
+		current = self.head
+
+		while current.next:
+			current = current.next
+
+		current.next = new_node
+
+	def display(self):
+		current = self.head
+		while True:
+			if current:
+				print(current.data, end='')
+				if current.next:
+					current = current.next
+					print(' -> ', end='')
+				else:
+					return
+
+
+ll = LinkedList()
+ll.append(1)
+ll.append(2)
+ll.append(3)
+ll.append(4)
+ll.append(3)
+
+ll.display()
